1ea91b95b82814b06b7377842dc46a01028f8f09
dgoldshteyn00 <45824229+dgoldshteyn00@users.noreply.github.com>
2020-02-10 20:59:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,33 +5,35 @@ s = new_screen()
 c = [0, 255, 0]
 
 # octants 1 and 5
-draw_line(0, 0, XRES - 1, YRES - 1, s, c)
-draw_line(0, 0, XRES - 1, YRES / 2, s, c)
-draw_line(XRES - 1, YRES - 1, 0, YRES / 2, s, c)
+
+
+draw_line(0, 0, X_RES - 1, Y_RES - 1, s, c)
+draw_line(0, 0, X_RES - 1, Y_RES / 2, s, c)
+draw_line(X_RES - 1, Y_RES - 1, 0, Y_RES / 2, s, c)
 
 # octants 8 and 4
 c[BLUE] = 255
-draw_line(0, YRES - 1, XRES - 1, 0, s, c)
-draw_line(0, YRES - 1, XRES - 1, YRES / 2, s, c)
-draw_line(XRES - 1, 0, 0, YRES / 2, s, c)
+draw_line(0, Y_RES - 1, X_RES - 1, 0, s, c)
+draw_line(0, Y_RES - 1, X_RES - 1, Y_RES / 2, s, c)
+draw_line(X_RES - 1, 0, 0, Y_RES / 2, s, c)
 
 # octants 2 and 6
 c[RED] = 255
 c[GREEN] = 0
 c[BLUE] = 0
-draw_line(0, 0, XRES / 2, YRES - 1, s, c)
-draw_line(XRES - 1, YRES - 1, XRES / 2, 0, s, c)
+draw_line(0, 0, X_RES / 2, Y_RES - 1, s, c)
+draw_line(X_RES - 1, Y_RES - 1, X_RES / 2, 0, s, c)
 
 # octants 7 and 3
 c[BLUE] = 255
-draw_line(0, YRES - 1, XRES / 2, 0, s, c)
-draw_line(XRES - 1, 0, XRES / 2, YRES - 1, s, c)
+draw_line(0, Y_RES - 1, X_RES / 2, 0, s, c)
+draw_line(X_RES - 1, 0, X_RES / 2, Y_RES - 1, s, c)
 
 # horizontal and vertical
 c[BLUE] = 0
 c[GREEN] = 255
-draw_line(0, YRES / 2, XRES - 1, YRES / 2, s, c)
-draw_line(XRES / 2, 0, XRES / 2, YRES - 1, s, c)
+draw_line(0, Y_RES / 2, X_RES - 1, Y_RES / 2, s, c)
+draw_line(X_RES / 2, 0, X_RES / 2, Y_RES - 1, s, c)
 
 # display(s) <more trouble than it's worth>
 save_ppm(s, 'binary.ppm')
